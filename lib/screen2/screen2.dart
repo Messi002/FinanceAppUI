e092df6903999fc4080eb182a80ed14ad1f45bbe
Messi002 <rostandmessi2@gmail.com>
2022-08-07 00:56:37 +0100

    financeApp

--- lib/screen2/screen2.dart
+++ lib/screen2/screen2.dart
@@ -1,3 +1,4 @@
+import 'package:app/utils/card1.dart';
 import 'package:app/utils/main_board_container.dart';
 import 'package:flutter/material.dart';
 
@@ -60,84 +61,12 @@ class _Screen2State extends State<Screen2> {
                   SizedBox(
                     height: 10,
                   ),
-                  Container(
-                    height: 70,
-                    padding: const EdgeInsets.symmetric(horizontal: 13.0),
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 208, 202, 202),
-                        borderRadius: BorderRadius.circular(16.0)),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            flex: 1,
-                            child: Container(
-                              height: 35,
-                              padding: EdgeInsets.fromLTRB(10, 5, 0, 5),
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: Row(children: [
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      '....',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    SizedBox(
-                                      height: 4,
-                                    ),
-                                    Text(
-                                      '3490',
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 10),
-                                    )
-                                  ],
-                                )
-                              ]),
-                            )),
-                        Expanded(
-                            flex: 3,
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 16.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "\$1,020.92",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  ),
-                                  SizedBox(
-                                    height: 2,
-                                  ),
-                                  Text("Visa",
-                                      style: TextStyle(
-                                          fontSize: 12,
-                                          color: Colors.grey[600])),
-                                ],
-                              ),
-                            )),
-                        Expanded(
-                          child: Icon(
-                            Icons.arrow_forward_ios,
-                            size: 18,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  Card1(text1: "3490", text2: "\$1,020.92", text3: "Visa", color: Colors.black),
                 ],
               ),
             )
-          ]),
+          ],
+          ),
         ),
       ),
     );
--- lib/utils/card1.dart
+++ lib/utils/card1.dart
@@ -8,13 +8,86 @@ class Card1 extends StatelessWidget {
       required this.text3,
       required this.color});
 
-  String text1;
-  String text2;
-  String text3;
-  Color color;
+ final String text1;
+ final  String text2;
+ final String text3;
+ final  Color color;
 
   @override
   Widget build(BuildContext context) {
-    return   ;
+    return   Container(
+                    height: 70,
+                    padding: const EdgeInsets.symmetric(horizontal: 13.0),
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 208, 202, 202),
+                        borderRadius: BorderRadius.circular(16.0)),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            flex: 1,
+                            child: Container(
+                              height: 35,
+                              padding: EdgeInsets.fromLTRB(10, 5, 0, 5),
+                              decoration: BoxDecoration(
+                                  color: color,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Row(children: [
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      '....',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      height: 4,
+                                    ),
+                                    Text(
+                                      text1,
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 10),
+                                    )
+                                  ],
+                                )
+                              ]),
+                            )),
+                        Expanded(
+                            flex: 3,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 16.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    text2,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                  SizedBox(
+                                    height: 2,
+                                  ),
+                                  Text(text3,
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.grey[600])),
+                                ],
+                              ),
+                            )),
+                        Expanded(
+                          child: Icon(
+                            Icons.arrow_forward_ios,
+                            size: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
   }
 }
