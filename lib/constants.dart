45b72c70a40e835e64e8da6ad8ee2f0686e10485
Messi002 <rostandmessi2@gmail.com>
2022-08-07 06:29:19 +0100

    financeApp

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const kBgDecoration = BoxDecoration(
     gradient: LinearGradient(
   colors: [
-    Color.fromARGB(255, 8, 76, 132),
+    Color.fromARGB(255, 15, 53, 114),
     Color.fromARGB(255, 15, 51, 115),
   ],
   begin: FractionalOffset(0, 0),
