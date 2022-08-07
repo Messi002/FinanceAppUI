947c047ff9c2651e3482f506e3f7c3b349242491
Messi002 <rostandmessi2@gmail.com>
2022-08-07 03:06:13 +0100

    financeApp

---
+++ lib/screen2/screen1.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatefulWidget {
+  const Screen1({Key? key}) : super(key: key);
+
+  @override
+  State<Screen1> createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: Container(
+          padding: const EdgeInsets.symmetric(horizontal:25.0 ,vertical: 10),
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    ));
+  }
+}
