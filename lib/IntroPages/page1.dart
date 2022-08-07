3b85841bbe0f340dde095bccdbaf327ba50cd49e
Messi002 <rostandmessi2@gmail.com>
2022-08-07 20:32:03 +0100

    financeApp

--- lib/IntroPages/page1.dart
+++ lib/IntroPages/page1.dart
@@ -11,16 +11,43 @@ class Page1 extends StatelessWidget {
     return Container(
       height: MediaQuery.of(context).size.height,
       decoration: kBgDecoration,
-      child: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.7,
-          ),
-          Container(
-            color: Colors.amber,
-            child: Text("hello world"),
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 1.9,
+            ),
+            Container(
+              child: Text(
+                "Best Way To \nTracking Your \nMoney...",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromARGB(255, 229, 223, 223),
+                  wordSpacing: 3,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 35,
+            ),
+            Container(
+              child: Text(
+                "Control and track what you spend \nyour money on",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[500],
+                  wordSpacing: 3,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
