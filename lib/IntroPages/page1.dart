bc7ba1491af723281ef13f5c29845935a252e2ca
Messi002 <rostandmessi2@gmail.com>
2022-08-07 05:45:42 +0100

    financeApp

--- lib/IntroPages/page1.dart
+++ lib/IntroPages/page1.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:app/constants.dart';
 import 'package:flutter/material.dart';
 
 class Page1 extends StatelessWidget {
@@ -8,17 +9,7 @@ class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          Color.fromARGB(255, 8, 76, 132),
-          Color.fromARGB(255, 15, 51, 115),
-        ],
-        begin: FractionalOffset(0, 0),
-        end: FractionalOffset(1.0, 0.0),
-        stops: [0.0, 1.0],
-        tileMode: TileMode.clamp,
-      )),
+      decoration: kBgDecoration,
     );
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
 
-const kBgDecoration = 
+const kBgDecoration = BoxDecoration(
+    gradient: LinearGradient(
+  colors: [
+    Color.fromARGB(255, 8, 76, 132),
+    Color.fromARGB(255, 15, 51, 115),
+  ],
+  begin: FractionalOffset(0, 0),
+  end: FractionalOffset(1.0, 0.0),
+  stops: [0.0, 1.0],
+  tileMode: TileMode.clamp,
+));
