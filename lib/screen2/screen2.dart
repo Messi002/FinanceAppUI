474e62afb4c17030aefbc5b4bc24d7c2248fb23d
Messi002 <rostandmessi2@gmail.com>
2022-08-07 01:15:02 +0100

    financeApp

--- lib/screen2/screen2.dart
+++ lib/screen2/screen2.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:app/utils/card1.dart';
 import 'package:app/utils/main_board_container.dart';
 import 'package:flutter/material.dart';
@@ -29,43 +31,76 @@ class _Screen2State extends State<Screen2> {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'Current balance',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text(
-                    '\$ 2,090.20',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w600, fontSize: 35),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  const MyCard(),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const Text('Your cards'),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Card1(text1: "3490", text2: "\$1,020.92", text3: "Visa", color: Colors.black),
-                ],
-              ),
-            )
-          ],
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Current balance',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Text(
+                      '\$ 2,090.20',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w600, fontSize: 35),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const MyCard(),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text('Your cards'),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Card1(
+                        text1: "3490",
+                        text2: "\$1,020.92",
+                        text3: "Visa",
+                        color: Colors.black),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Card1(
+                        text1: "2820",
+                        text2: "\$1,890.30",
+                        text3: "Master Card",
+                        color: Color.fromARGB(255, 39, 27, 166)),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Text("Expences"),
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            border: Border.all(color: Colors.black),
+                          ),
+                          child: Text(
+                            "View All",
+                            style: TextStyle(
+                                fontSize: 10, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
