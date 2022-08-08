0bbb81e77c6f4ac7a29e73666b0e4f79b7beefd6
Messi002 <rostandmessi2@gmail.com>
2022-08-08 13:04:31 +0100

    financeApp

--- lib/screens/screen3.dart
+++ lib/screens/screen3.dart
@@ -146,5 +146,16 @@ class _Screen3State extends State<Screen3> {
     );
   }
 
+  Expanded NumDisplay(String a, String b, String c) {
+    return Expanded(
+        flex: 0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(a, style: kTextStyle),
+            Text(b, style: kTextStyle),
+            Text(c, style: kTextStyle),
+          ],
+        ));
   }
 }
