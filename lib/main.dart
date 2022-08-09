6c371795f844096887c4ee3908802777e9b61428
Messi002 <rostandmessi2@gmail.com>
2022-08-09 14:49:29 +0100

    financeApp

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,4 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      ),
-      home:const Screen1(),
-    );
-  }
-}
+  Widg
