5425bc8d2d708cf4b0d60a0316368032f64a6c29
Messi002 <rostandmessi2@gmail.com>
2022-08-09 14:52:02 +0100

    financeApp

--- lib/screens/screen3.dart
+++ lib/screens/screen3.dart
@@ -115,7 +115,18 @@ class _Screen3State extends State<Screen3> {
                     flex: 0,
                     child: MyButton(
                       text: "Continue",
-              
+                      color: Colors.black,
+                      textColor: Colors.white,
+                      isExtended: false,
+                    )),
+                SizedBox(
+                  height: 30,
+                ),
+                NumDisplay("1", "2", "3"),
+                SizedBox(
+                  height: 16,
+                ),
+                NumDisplay("4", "5", "6"),
                 SizedBox(
                   height: 16,
                 ),
@@ -138,12 +149,6 @@ class _Screen3State extends State<Screen3> {
   Expanded NumDisplay(String a, String b, String c) {
     return Expanded(
         flex: 0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text(a, style: kTextStyle),
-            Text(b, style: kTextStyle),
-            Text(c, style: kTextStyle),
           ],
         ));
   }
